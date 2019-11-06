e3f00ebaee2afc83b17b034091a24e3286ba7b8e
anheew1 <anheew1@kookmin.ac.kr>
2019-11-06 12:48:32 +0900

    Add SceneChanger to StartScene

---
+++ Parking Game/Assets/Scripts/Start/SceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void changeToSelectStage()
+    {
+        SceneManager.LoadScene("SelectStage");
+    }
+    public void changeToStart()
+    {
+        SceneManager.LoadScene("Start");
+    }
+    public void changeToSample()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
