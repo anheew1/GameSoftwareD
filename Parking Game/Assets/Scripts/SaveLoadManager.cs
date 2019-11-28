da1849a929895550563adf083371759f1e7bd286
anheew1 <anheew1@kookmin.ac.kr>
2019-11-29 01:25:09 +0900

    create SaveLoadManager

---
+++ Parking Game/Assets/Scripts/SaveLoadManager.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+public class GameData
+{
+    public string stage;
+    public string score;
+    public string time;
+
+    public GameData(string stage,string score,string time)
+    {
+        this.stage = stage;
+        this.score = score;
+        this.time = time;
+    }
+}
+
+public class SaveLoadManager : MonoBehaviour
+{
+    private static List<GameData> dataList = new List<GameData>();
+    public static string m_time = "";
+    public static string m_score = "";
+    // Update is called once per frame
+
+    public static List<GameData> Load()
+    {
+        
+        string filePath = Application.dataPath + "/Resources" + "/gameData.json";
+
+        CheckAndCreateFile();
+        
+        string jsonString = File.ReadAllText(filePath);
+
+        JsonData jsonData = JsonMapper.ToObject(jsonString);
+
+        for(int i=0;i< jsonData.Count;i++)
+        {
+            GameData data = new GameData(jsonData[i]["stage"].ToString(), 
+                jsonData[i]["score"].ToString(), jsonData[i]["time"].ToString());
+
+            dataList.Add(data);
+        }
+
+        return dataList;
+    }
+    public List<GameData> getDataList()
+    {
+        return dataList;
+    }
+    public static void Save(string stage,string score="",string time = "")
+    {
+
+        if(score == "")
+        {
+            score = m_score;
+        }
+        if(time == "")
+        {
+            time = m_time;
+        }
+
+        string filePath = Application.dataPath + "/Resources" + "/gameData.json";
+        CheckAndCreateFile();
+
+        GameData data = new GameData(stage, score, time);
+        bool isDataExists = false;
+        for(int i = 0; i < dataList.Count; i++)
+        {
+            if (data.stage.Equals(dataList[i].stage))
+            {
+                dataList[i] = data;
+                isDataExists = true;
+                break;
+            }
+        }
+        if (!isDataExists)
+        {
+            dataList.Add(data);
+        }
+        
+        JsonData jsonData = JsonMapper.ToJson(dataList);
+
+        File.WriteAllText(filePath, jsonData.ToString());
+    }
+    private static void CheckAndCreateFile()
+    {
+        string directoryPath = Application.dataPath + "/Resources";
+        string filePath = directoryPath + "/gameData.json";
+        if (!File.Exists(filePath))
+        {
+            Directory.CreateDirectory(directoryPath);
+            File.Create(filePath).Dispose();
+        }
+        
+        
+    }
+}
