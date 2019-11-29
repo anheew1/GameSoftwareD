a0f687a9dec26effd15b7fae78598fafb8ecaeaa
anheew1 <anheew1@kookmin.ac.kr>
2019-11-30 02:47:39 +0900

    Now You can Load data at STart

--- Parking Game/Assets/Scripts/Start/StartUI.cs
+++ Parking Game/Assets/Scripts/Start/StartUI.cs
@@ -31,7 +31,41 @@ public class StartUI : MonoBehaviour
             isUIopen = true;
             selectMap.SetActive(true);
         }
-        
+        if (isUIopen)
+        {
+            MarkStarsAndTimes();
+        }
+    }
+
+    private void MarkStarsAndTimes()
+    {
+        if (dataList == null) return;
+        for(int gi=0;gi< dataList.Count;gi++)
+        {
+            GameData gameData = dataList[gi];
+            GameObject stageLabel;
+            if (gameData.stage.Length > 1) {
+                stageLabel = GameObject.Find("Stage" + gameData.stage);
+            }
+            else
+            {
+                stageLabel = GameObject.Find("Stage0" + gameData.stage);
+            }
+
+            Image[] stars = stageLabel.transform.Find("Stars").GetComponentsInChildren<Image>();
+            int score = int.Parse(gameData.score);
+            for(int i =0; i < score; i++)
+            {
+                stars[i].color = new Color(255, 255, 0, 255); // Yellow
+            }
+
+            Text timeText = stageLabel.transform.Find("Time").GetComponent<Text>();
+            float time = float.Parse(gameData.time);
+            int minute = (int) time / 60;
+            time -= 60 * minute;
+            timeText.text = minute + ":" + time.ToString("0.00");
+
+        }
     }
     
 }
