6a22f718cbd10200f1e41fb4c7c8810bbb8c839d
anheew1 <anheew1@kookmin.ac.kr>
2019-11-30 02:46:38 +0900

    fixed InvalidOperation error

--- Parking Game/Assets/Scripts/SaveLoadManager.cs
+++ Parking Game/Assets/Scripts/SaveLoadManager.cs
@@ -34,6 +34,10 @@ public class SaveLoadManager : MonoBehaviour
         CheckAndCreateFile();
         
         string jsonString = File.ReadAllText(filePath);
+        if (jsonString.Equals(""))
+        {
+            return dataList;
+        }
 
         JsonData jsonData = JsonMapper.ToObject(jsonString);
 
